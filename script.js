a3e5e6be8da8b023e8ff20849a14d85bd76f75b6
ShanchaiZ <shanchai.zahid@gmail.com>
2022-10-15 19:59:23 -0400

    added HTML markup for score display and player1,player 2, and Reset button

--- script.js
+++ script.js
@@ -1,1 +1,8 @@
 console.log("hello")
+
+//PseudoCode:
+//==================================================
+//HTML SET UP: Create the base button set up
+//----------------------------------------------------
+//1. Create h1 display where the score will be displayed. Span and id was used to isolate left and right score point.
+//2. Create Increase Player 1, 2 and Reset buttons
