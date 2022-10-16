b03fd4fd1327c5ca99f53934cec51b2d02577436
ShanchaiZ <shanchai.zahid@gmail.com>
2022-10-16 13:05:23 -0400

    added scorekeeping logic of ANY player reaching winningscore thus ending game as a Boolean. Same logic repeated for player 2.

--- script.js
+++ script.js
@@ -9,29 +9,36 @@ const p2Display = document.querySelector("#p2Display");
 let p1Score = 0;
 let p2Score = 0;
 let winningScore = 5;
-let isGameOver = false; 
+let isGameOver = false;
 
 
 
 //Player Button Functions:
 //----------------------------------------------------------------------
 p1Button.addEventListener("click", function () {
-    if (p1Score !== winningScore) {
+    if (!isGameOver) {
         p1Score += 1;
+        if (p1Score === winningScore) {
+            isGameOver = true;
+        }
         p1Display.textContent = p1Score;
     }
 });
 
+
 p2Button.addEventListener("click", function () {
-    if (p2Score !== winningScore) {
+    if (!isGameOver) {
         p2Score += 1;
+        if (p2Score === winningScore) {
+            isGameOver = true;
+        }
         p2Display.textContent = p2Score;
     }
 });
 
 
 
-//PseudoCode:
+//PSEUDOCODE:
 //==================================================
 //HTML SET UP: Create the base button set up
 //----------------------------------------------------
@@ -42,10 +49,16 @@ p2Button.addEventListener("click", function () {
 //==================================================
 //3. Select player display and buttons with document.queryselector.
 
-//Create Scorekeepering logic: Take the current score for player 1 then increase it by 1 and then update it on the display for player 1. Once player 1 reaches the winning score = gameover. Repeat logic with player 2.
+//Scorekeepering logic 1: Take the current score for player 1 then increase it by 1 and then update it on the display for player 1. Once player 1 reaches the winning score = gameover. Repeat logic with player 2.
 //----------------------------------------------------
 //4. Select the player 1 Score using querySelector.
 //5. let player 1 score = 0.
-//6. After each button click on player 1, INCREASE the p1Score by 1 and then change its TEXT CONTENT to update.
-//7. Once player 1 reaches the winningScore = ScoreKeeping stops. 
+//6. After each button click on player 1, INCREASE the p1Score by 1 and then change its TEXT CONTENT to update diplay.
+//7. Once player 1 reaches the winningScore = ScoreKeeping stops.
 //8. REWRITE step 5- 7 as if you DO NOT have the winning score continue with incremental scorekeeping; BUT once player 1 reaches winning score === stop running code.
+
+//Scorekeepering logic 2: Once ANY player reaches the game winning score === gameover. Therefore, Another way to think about it is, are we we STILL playing the game once the winningScore is reached ==> Boolean logic.
+//----------------------------------------------------
+//9. let isGameOver === false because we are still playing the game and continue to increase score by 1 and update display for player.
+//10. Once any player score === winning score, isGameOver will be === true. then none of the code will run.
+//11. repeat for player 2.
