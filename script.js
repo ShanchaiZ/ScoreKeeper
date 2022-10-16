c4521161909818561e9e31c40c215967e90db513
ShanchaiZ <shanchai.zahid@gmail.com>
2022-10-16 12:18:24 -0400

    Rewrote scorekeeper logic for player 1 using Boolean operator and hardcoded winningScore to 5 for initial testing

--- script.js
+++ script.js
@@ -7,18 +7,21 @@ const p1Display = document.querySelector("#p1Display");
 const p2Display = document.querySelector("#p2Display");
 
 let p1Score = 0;
+let winningScore = 5;
+let isGameOver = false; 
 
 
 
-
-//Button Functions:
+//Player Button Functions:
 //----------------------------------------------------------------------
-p1Button.addEventListener("click", function() {
-    p1Score += 1;
-    p1Display.textContent = p1Score;
+p1Button.addEventListener("click", function () {
+    if (p1Score !== winningScore) {
+        p1Score += 1;
+        p1Display.textContent = p1Score;
+    }
 });
 
-p2Button.addEventListener("click", function() {
+p2Button.addEventListener("click", function () {
     alert("p2 Is working!")
 });
 
@@ -29,13 +32,16 @@ p2Button.addEventListener("click", function() {
 //HTML SET UP: Create the base button set up
 //----------------------------------------------------
 //1. Create h1 display where the score will be displayed. Span and id was used to isolate left and right score point.
-//2. Create Increase Player 1, 2 and Reset buttons
-//----------------------------------------------------
-//JS Script: BUTTON SELECTION AND FUNCTION:
-//3. Select buttons with document.queryselector
+//2. Create Increase Player 1, 2 and Reset buttons.
 
-//Create Scorekeepering logic: We want to take the current score for player 1 then increase it by 1 and then update it on the display for player 1.
+//JS Script: BUTTON SELECTION AND FUNCTION:
+//==================================================
+//3. Select player display and buttons with document.queryselector.
+
+//Create Scorekeepering logic: Take the current score for player 1 then increase it by 1 and then update it on the display for player 1. Once player 1 reaches the winning score = gameover.
 //----------------------------------------------------
-//4. Select the player 1 Display using querySelector
-//5. let player 1 score = 0. 
-//6. Then after each button click on player 1, INCREASE the p1Score by 1 and then change its TEXT CONTENT to update.
+//4. Select the player 1 Score using querySelector.
+//5. let player 1 score = 0.
+//6. After each button click on player 1, INCREASE the p1Score by 1 and then change its TEXT CONTENT to update.
+//7. Once player 1 reaches the winningScore = ScoreKeeping stops. 
+//8. REWRITE step 5- 7 as if you DO NOT have the winning score continue with incremental scorekeeping; BUT once player 1 reaches winning score === stop running code.
