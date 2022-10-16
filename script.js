5dc6f45823ca384f3e7f60758592def797d1d212
ShanchaiZ <shanchai.zahid@gmail.com>
2022-10-15 20:18:34 -0400

    added and verified buttons connection in js

--- script.js
+++ script.js
@@ -1,4 +1,30 @@
-console.log("hello")
+//Score Keeping App Functions:
+//====================================================================================
+//Declare variables:
+const p1Button = document.querySelector("#p1Button");
+const p2Button = document.querySelector("#p2Button");
+
+
+
+
+
+
+
+
+
+
+
+//Button Functions:
+//----------------------------------------------------------------------
+p1Button.addEventListener("click", function() {
+    alert("p1 Is working!")
+});
+
+p2Button.addEventListener("click", function() {
+    alert("p2 Is working!")
+});
+
+
 
 //PseudoCode:
 //==================================================
@@ -6,3 +32,6 @@ console.log("hello")
 //----------------------------------------------------
 //1. Create h1 display where the score will be displayed. Span and id was used to isolate left and right score point.
 //2. Create Increase Player 1, 2 and Reset buttons
+//----------------------------------------------------
+//JS Script: BUTTON SELECTION AND FUNCTION:
+//3. Select buttons with document.queryselector
