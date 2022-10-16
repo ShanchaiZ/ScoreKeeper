b74b3926a17a916e7ed3d06ad0690795d9dd11fd
ShanchaiZ <shanchai.zahid@gmail.com>
2022-10-16 11:48:30 -0400

    added scorekeeping logic for player 1

--- script.js
+++ script.js
@@ -3,13 +3,10 @@
 //Declare variables:
 const p1Button = document.querySelector("#p1Button");
 const p2Button = document.querySelector("#p2Button");
+const p1Display = document.querySelector("#p1Display");
+const p2Display = document.querySelector("#p2Display");
 
-
-
-
-
-
-
+let p1Score = 0;
 
 
 
@@ -17,7 +14,8 @@ const p2Button = document.querySelector("#p2Button");
 //Button Functions:
 //----------------------------------------------------------------------
 p1Button.addEventListener("click", function() {
-    alert("p1 Is working!")
+    p1Score += 1;
+    p1Display.textContent = p1Score;
 });
 
 p2Button.addEventListener("click", function() {
@@ -35,3 +33,9 @@ p2Button.addEventListener("click", function() {
 //----------------------------------------------------
 //JS Script: BUTTON SELECTION AND FUNCTION:
 //3. Select buttons with document.queryselector
+
+//Create Scorekeepering logic: We want to take the current score for player 1 then increase it by 1 and then update it on the display for player 1.
+//----------------------------------------------------
+//4. Select the player 1 Display using querySelector
+//5. let player 1 score = 0. 
+//6. Then after each button click on player 1, INCREASE the p1Score by 1 and then change its TEXT CONTENT to update.
